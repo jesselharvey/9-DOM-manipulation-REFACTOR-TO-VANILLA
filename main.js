f208c04ae7a7f3564993c4ca395c872e4d74111d
Jesse Harvey <jesselharvey@gmail.com>
2020-09-05 15:18:24 -0700

    added click to toggle, java

--- main.js
+++ main.js
@@ -3,19 +3,22 @@
 // or mouseover and mouseout      /
 
 // const second = document.querySelector("#second")
+
 document.querySelector("#main").addEventListener("click", function (e) {
   const header = e.target.className.includes("outter")
+  const idSelector = `#` + e.target.id + `-a`
   if (header) {
-    const idSelector = e.target.id
     console.log(idSelector)
-    document.querySelector("idSelector").removeAttribute("display")
+    document.querySelector(idSelector).classList.toggle("hide")
   }
-
-  // if (change.classList.contains("outterA")) {
-  //   change.classList.add("innerA")
-  //   change.classList.remove("inner1")
+  // else {
+  //   document.querySelector(idSelector).classList.toggle("hide")
   // }
 })
+// if (change.classList.contains("outterA")) {
+//   change.classList.add("innerA")
+//   change.classList.remove("inner1")
+// }
 
 // change.classList.add("containerA")
 // change.classList.remove("container1")
